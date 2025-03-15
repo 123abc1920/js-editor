710289f345a9665ab018dd1e45f503b427378268
123abc1920 <123abc1920@mail.ru>
2025-03-15 23:19:39 +0300

    add drawing

--- mainjs.js
+++ mainjs.js
@@ -1,9 +1,42 @@
 let isDraw = false;
+let draw = false;
 var canvas = document.getElementById("canvas");
-canvas.width = 100;
-canvas.height = 100;
+canvas.width = 1000;
+canvas.height = 1000;
+const rect = canvas.getBoundingClientRect();
+canvas.width = rect.width;
+canvas.height = rect.height;
 var ctx = canvas.getContext("2d");
+ctx.fillStyle = "white";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
+var mouse = { x: 0, y: 0 };
+
+canvas.addEventListener("mousedown", function (e) {
+    mouse.x = e.pageX - this.offsetLeft;
+    mouse.y = e.pageY - this.offsetTop;
+    draw = true;
+    ctx.beginPath();
+    ctx.moveTo(mouse.x, mouse.y);
+});
+
+canvas.addEventListener("mousemove", function (e) {
+    if (isDraw && draw) {
+        mouse.x = e.pageX - this.offsetLeft;
+        mouse.y = e.pageY - this.offsetTop;
+        ctx.lineTo(mouse.x, mouse.y);
+        ctx.stroke();
+    }
+});
+
+canvas.addEventListener("mouseup", function (e) {
+    mouse.x = e.pageX - this.offsetLeft;
+    mouse.y = e.pageY - this.offsetTop;
+    if (isDraw && draw) {
+        ctx.lineTo(mouse.x, mouse.y);
+    }
+    draw = false;
+    ctx.stroke();
+});
 
 function openFile(event) {
     event.preventDefault();
@@ -32,15 +65,18 @@ function openImg(url) {
     const img = new Image();
     img.src = url;
     img.onload = () => {
-        canvas.width = img.width;
-        canvas.height = img.height;
-        ctx.drawImage(img, 0, 0);
-
         if (img.width >= 2 * img.height) {
             canvas.setAttribute('style', 'height: auto; width: 100%;');
         } else {
             canvas.setAttribute('style', 'height: 100%; width: auto;');
         }
+        canvas.width = img.width;
+        canvas.height = img.height;
+        const rect = canvas.getBoundingClientRect();
+        canvas.width = rect.width;
+        canvas.height = rect.height;
+        ctx = canvas.getContext("2d");
+        ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, canvas.width, canvas.height);
     }
 }
 

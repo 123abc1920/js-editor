5275dfebe928f52f1599a524202c99cefc4fce60
123abc1920 <123abc1920@mail.ru>
2025-04-06 14:27:20 +0300

    add scaling

--- scripts/files.js
+++ scripts/files.js
@@ -1,14 +1,12 @@
 var canvas = document.getElementById("canvas");
-canvas.width = 1000;
-canvas.height = 1000;
 const rect = canvas.getBoundingClientRect();
-canvas.width = rect.width;
-canvas.height = rect.height;
 var ctx = canvas.getContext("2d");
-ctx.fillStyle = "white";
-ctx.fillRect(0, 0, canvas.width, canvas.height);
+newFile(null);
 
+var scalingCanvas = 1;
 var img = null;
+var imgWidth = 1000;
+var imgHeight = 1000;
 
 function openFile(event) {
     event.preventDefault();
@@ -22,15 +20,29 @@ function openFile(event) {
 }
 
 function newFile(event) {
-    event.preventDefault();
-    canvas.width = 1000;
-    canvas.height = 1000;
-    const rect = canvas.getBoundingClientRect();
-    canvas.width = rect.width;
-    canvas.height = rect.height;
-    var ctx = canvas.getContext("2d");
+    imgHeight = 1000;
+    imgWidth = 1000;
+    const rect = imgPanel.getBoundingClientRect();
+    var scale = Math.min(rect.width / imgWidth, rect.height / imgHeight);
+    var newWidth;
+    var newHeight;
+    if (imgWidth > imgHeight) {
+        newWidth = rect.width;
+        newHeight = imgHeight * scale;
+    } else if (imgWidth < imgHeight) {
+        newHeight = rect.height;
+        newWidth = imgWidth * scale;
+    } else if (imgWidth == imgHeight) {
+        newHeight = rect.height;
+        newWidth = imgWidth * scale;
+    }
+
+    canvas.width = newWidth;
+    canvas.height = newHeight;
+    ctx = canvas.getContext("2d");
     ctx.fillStyle = "white";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
+
     undo.length = 0;
     redo.length = 0;
 }
@@ -54,36 +66,41 @@ function openImg(url) {
     img.onload = () => {
         var imgPanel = document.getElementById("imgPanel");
         const rect = imgPanel.getBoundingClientRect();
+        imgWidth = img.width;
+        imgHeight = img.height;
 
-        canvas.width = rect.width;
-        canvas.height = rect.height;
+        var scale = Math.min(rect.width / imgWidth, rect.height / imgHeight);
+        var newWidth;
+        var newHeight;
+        if (imgWidth > imgHeight) {
+            newWidth = rect.width;
+            newHeight = imgHeight * scale;
+        } else if (imgWidth <= imgHeight) {
+            newHeight = rect.height;
+            newWidth = imgWidth * scale;
+        }
 
-        var scale = Math.min(rect.width / img.width, rect.height / img.height);
-
-        var newWidth = img.width * scale;
-        var newHeight = img.height * scale;
-
-        var xOffset = (rect.width - newWidth) / 2;
-        var yOffset = (rect.height - newHeight) / 2;
-
+        canvas.width = newWidth;
+        canvas.height = newHeight;
         ctx = canvas.getContext("2d");
-        ctx.drawImage(img, 0, 0, img.width, img.height, xOffset, yOffset, newWidth, newHeight);
+        ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, newWidth, newHeight);
     }
     undo.length = 0;
     redo.length = 0;
 }
 
-function resizeCanvas() {
-    const canvasAspectRatio = canvas.width / canvas.height;
-    const windowAspectRatio = window.innerWidth / window.innerHeight;
-    const width = window.innerWidth;
-    const height = width / canvasAspectRatio;
+function resizeCanvas(event, value) {
+    document.getElementById('size-picker').value = value;
+    document.getElementById('size-text').value = value;
 
-    if (windowAspectRatio < canvasAspectRatio) {
-        canvas.width = window.innerHeight * canvasAspectRatio;
-        canvas.height = window.innerHeight;
-    } else {
-        canvas.width = width;
-        canvas.height = height;
-    }
+    scalingCanvas = value / 100;
+
+    var imgPanel = document.getElementById("imgPanel");
+    const rect = imgPanel.getBoundingClientRect();
+    var newWidth = imgWidth * scalingCanvas;
+    var newHeight = imgHeight * scalingCanvas;
+    canvas.width = newWidth;
+    canvas.height = newHeight;
+    ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0, imgWidth, imgHeight, 0, 0, newWidth, newHeight);
 }

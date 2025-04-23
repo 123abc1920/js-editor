878f5e0ec795d347e4fed8dd7b4894086eee8392
123abc1920 <123abc1920@mail.ru>
2025-04-24 01:03:58 +0300

    add adresses

--- app.js
+++ app.js
@@ -2,10 +2,21 @@ const express = require("express");
 var path = require('path');
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname));
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/views/main.html');
+});
+
+app.post('/profile', (req, res) => {
+    const username = req.body.username;
+    const userpass = req.body.userpass;
+    res.sendFile(path.join(__dirname + '/views/profile.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname + '/views/login.html'));
 });
 
 app.listen(3000);

195c7d6186ad2b0109432dbcfb3e968598e5f269
123abc1920 <123abc1920@mail.ru>
2025-05-04 15:30:51 +0300

    add new color picker

--- public/js/penprefs.js
+++ public/js/penprefs.js
@@ -3,11 +3,23 @@ var ctx = canvas.getContext("2d");
 
 var fill = false;
 
-$(document).ready(function () {
-    $('#color-picker').colorpicker({
-        inline: true,
-        container: true
-    });
+var joe = colorjoe.rgb("rgbPicker", '#113c38', [
+    'currentColor',
+    ['fields', { space: 'RGB', limit: 255, fix: 2 }],
+    'hex',
+    'text',
+    ['text', { text: 'param demo' }]
+]);
+
+document.getElementsByClassName("twod")[0].style.width = "80%";
+document.getElementsByClassName("twod")[0].style.aspectRatio = "1/1";
+document.getElementsByClassName("twod")[0].style.height = "auto";
+
+joe.on("change", function (color) {
+    document.getElementsByClassName("twod")[0].style.width = "80%";
+    document.getElementsByClassName("twod")[0].style.aspectRatio = "1/1";
+    document.getElementsByClassName("twod")[0].style.height = "auto";
+    console.log("Selecting " + color.css());
 });
 
 function setNewColor(event, color) {
